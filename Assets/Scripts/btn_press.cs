ccaf85aebb27a3eacd31a3ff0273c8aba579d928
Anthony Desktop <aventu6@uic.edu>
2020-10-10 00:11:49 -0500

    alpha build
    Desktop deliverable mostly done, Desk collisions are done, button is implemented not finished

---
+++ Assets/Scripts/btn_press.cs
@@ -0,0 +1,29 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class btn_press : MonoBehaviour
+{
+
+    public GameObject button;
+
+    void Start()
+    {
+        button.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
+        button.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb) {
+
+        Debug.Log("Pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb) {
+
+        Debug.Log("Releassed");
+
+    }
+
+}
