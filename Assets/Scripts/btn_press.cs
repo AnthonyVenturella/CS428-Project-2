7277913de4dd3d7522c5f4c7a9b35807bc7f1027
Anthony Desktop <aventu6@uic.edu>
2020-10-10 19:01:23 -0500

    Alpha finished

--- Assets/Scripts/btn_press.cs
+++ Assets/Scripts/btn_press.cs
@@ -1,29 +1,42 @@
 ﻿using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using Vuforia;
 
-public class btn_press : MonoBehaviour
-{
+public class btn_press : MonoBehaviour{
 
     public GameObject button;
+    public AudioClip spawnSound;
+    public AudioSource m_MyAudioSource;
 
-    void Start()
-    {
+    public Animator anim;
+
+    void Start(){
+
         button.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
         button.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+
     }
 
     public void OnButtonPressed(VirtualButtonBehaviour vb) {
 
         Debug.Log("Pressed");
+        anim.SetTrigger("TypeToSit");
+        m_MyAudioSource.Play();
+        StartCoroutine(sitdown());
+
     }
 
     public void OnButtonReleased(VirtualButtonBehaviour vb) {
 
         Debug.Log("Releassed");
+    }
 
+    IEnumerator sitdown() {
+        yield return new WaitForSeconds(2);
+        anim.SetTrigger("SitToType");
     }
 
 }
